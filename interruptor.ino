3ff9911c182fad3095390f334a86db04011eec49
Michelle Gomes <gmichele498@gmail.com>
2024-11-27 19:47:16 -0300

    Update interruptor.ino

--- interruptor.ino
+++ interruptor.ino
@@ -1,27 +1,14 @@
-int botao = 0;
-int ultimoEstadoBotao = 0;
-int estadoLED = LOW;
-
-void setup() {
-  pinMode(7, INPUT);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, estadoLED);  // Configura o LED para o estado inicial
+void setup()
+{
+  pinMode(3, INPUT);
+  pinMode(12, OUTPUT);
 }
 
-void loop() {
-  // Lê o estado atual do botão
-  botao = digitalRead(7);
-  
-  // Verifica se o botão foi pressionado (estado mudou de HIGH para LOW)
-  if (botao == LOW && ultimoEstadoBotao == HIGH) {
-    // Inverte o estado do LED
-    estadoLED = !estadoLED;
-    digitalWrite(13, estadoLED);  // Atualiza o estado do LED
+void loop()
+{
+  if (digitalRead(3) == 1) {
+    digitalWrite(12, HIGH);
+  } else {
+    digitalWrite(12, LOW);
   }
-  
-  // Atualiza o último estado do botão
-  ultimoEstadoBotao = botao;
-
-  // Pequeno delay para evitar bouncing do botão
-  delay(50);
 }
