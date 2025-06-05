337e8fe4ddb9d8bdbf85d283346b8917b8fbbe84
Michelle Gomes <gmichele498@gmail.com>
2025-06-04 22:46:51 -0300

    Create sensor_LDR_LED.ino

---
+++ sensor_LDR_LED.ino
@@ -0,0 +1,25 @@
+#define SENSOR A2
+#define LED 13
+
+int valorSensor = 0;
+  
+void setup()
+{
+  pinMode(SENSOR, INPUT);
+  pinMode(LED, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  valorSensor = analogRead(SENSOR);
+  
+  if (valorSensor > 800)
+    digitalWrite(LED, HIGH);
+  else
+    digitalWrite(LED, LOW);
+  
+  Serial.println(valorSensor);
+  delay(1000);
+}
